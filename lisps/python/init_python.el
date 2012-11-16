31ecf0fe8c64c51bf52b6118f966f34aac494219
weikent <wei.shijian@gmail.com>
2012-11-16 21:25:25 +0800

    add ipython

--- lisps/python/init_python.el
+++ lisps/python/init_python.el
@@ -13,29 +13,29 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
 (ac-config-default)
 (global-auto-complete-mode t)  
-  
+
 ;;(setq-default ac-sources '(ac-source-words-in-same-mode-buffers))  
 (setq-default ac-sources '(ac-source-yasnippet    
-                 ac-source-semantic  
-;;                 ac-source-ropemacs  
-                 ac-source-imenu    
-                 ac-source-words-in-buffer  
-                 ac-source-dictionary  
-                 ac-source-abbrev    
-                 ac-source-words-in-buffer    
-                 ac-source-files-in-current-dir    
-                 ac-source-filename))   
-  
+			   ac-source-semantic  
+			   ;;                 ac-source-ropemacs  
+			   ac-source-imenu    
+			   ac-source-words-in-buffer  
+			   ac-source-dictionary  
+			   ac-source-abbrev    
+			   ac-source-words-in-buffer    
+			   ac-source-files-in-current-dir    
+			   ac-source-filename))   
+
 (add-hook 'emacs-lisp-mode-hook    (lambda () (add-to-list 'ac-sources 'ac-source-symbols)))  
 (add-hook 'auto-complete-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-filename)))  
 
 (add-hook 'python-mode-hook        (lambda () (add-to-list 'ac-omni-completion-sources (cons "\\." '(ac-source-ropemacs)))  ))    
-  
-  
+
+
 (set-face-background 'ac-candidate-face "lightgray")  
 (set-face-underline 'ac-candidate-face "darkgray")  
 (set-face-background 'ac-selection-face "steelblue")   
-  
+
 (setq ac-auto-start 2)  
 (setq ac-dwim t)  
 
@@ -44,15 +44,15 @@
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (require 'python-mode)
 (add-hook 'python-mode-hook
-      (lambda ()
-        (set-variable 'py-indent-offset 4)
-        ;(set-variable 'py-smart-indentation nil)
-        (set-variable 'indent-tabs-mode nil)
-        (define-key py-mode-map (kbd "RET") 'newline-and-indent)
-        ;(define-key py-mode-map [tab] 'yas/expand)
-        ;(setq yas/after-exit-snippet-hook 'indent-according-to-mode)
-        (smart-operator-mode-on)
-        ))
+	  (lambda ()
+	    (set-variable 'py-indent-offset 4)
+					;(set-variable 'py-smart-indentation nil)
+	    (set-variable 'indent-tabs-mode nil)
+	    (define-key py-mode-map (kbd "RET") 'newline-and-indent)
+					;(define-key py-mode-map [tab] 'yas/expand)
+					;(setq yas/after-exit-snippet-hook 'indent-according-to-mode)
+	    (smart-operator-mode-on)
+	    ))
 ;; pymacs
 (autoload 'pymacs-apply "pymacs")
 (autoload 'pymacs-call "pymacs")
@@ -65,11 +65,22 @@
 (setq ropemacs-enable-autoimport t)
 
 
+(require 'anything) (require 'anything-ipython)
+(when (require 'anything-show-completion nil t)
+  (use-anything-show-completion 'anything-ipython-complete
+				'(length initial-pattern)))
+
+(require 'comint)
+(define-key comint-mode-map (kbd "M-") 'comint-next-input)
+(define-key comint-mode-map (kbd "M-") 'comint-previous-input)
+(define-key comint-mode-map [down] 'comint-next-matching-input-from-input)
+(define-key comint-mode-map [up] 'comint-previous-matching-input-from-input)
+
 ;; (require 'pycomplete)  
 ;; (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))  
 ;; (setq interpreter-mode-alist(cons '("python2.7" . python-mode)  
 ;;                            interpreter-mode-alist))  
-  
+
 ;; (setq py-python-command "python2.7")  
 ;; (autoload 'python-mode "python-mode" "Python editing mode." t)  
 
