771bf08b63636f66cf690beba788de926e3dcef4
weikent <wei.shijian@gmail.com>
2012-12-17 16:13:20 +0800

    add iimage

--- lisps/orgMode-settings.el
+++ lisps/orgMode-settings.el
@@ -49,4 +49,17 @@
          )))
 
 (setq org-startup-indented t)
+
+
+(autoload 'iimage-mode "iimage" "Support Inline image minor mode." t)
+(autoload 'turn-on-iimage-mode "iimage" "Turn on Inline image minor mode." t)
+
+(defun org-toggle-iimage-in-org ()
+  "display images in your org file"
+  (interactive)
+  (if (face-underline-p 'org-link)
+      (set-face-underline-p 'org-link nil)
+      (set-face-underline-p 'org-link t))
+  (iimage-mode))
+
 (provide 'orgMode-settings)
