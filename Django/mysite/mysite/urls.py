0c37cf42d76ae8ab64d7372a7725f0e82eee6152
weikent <wei.shijian@gmail.com>
2013-03-22 16:31:23 +0800

    add Django

---
+++ Django/mysite/mysite/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+from mysite.views import hello
+from mysite.views import current_datetime
+from mysite.views import hours_ahead
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+                       (r'^hello/$', hello),
+                       (r'^current_datetime/$', current_datetime),
+                       (r'^time/plus/(\d{1,2})/$', hours_ahead),
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ Django/mysite/mysite/views.py
@@ -0,0 +1,40 @@
+from django.http import Http404, HttpResponse
+import datetime
+
+def hello(request):
+    """
+    
+    Arguments:
+    - `request`:
+    """
+    return HttpResponse("Hello World")
+
+
+def current_datetime(request):
+    """
+    
+    Arguments:
+    - `request`:
+    """
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" %now
+    return HttpResponse(html)
+
+
+def hours_ahead(request, offset):
+    """
+    
+    Arguments:
+    - `request`:
+    - `offset`:
+    """
+    try:
+        offset = int(offset)
+    except:
+        raise Http404()
+
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+
+    return HttpResponse(html)
