605ec84dc24adcc8a9961387763bc1bb3d32ca2f
weikent <wei.shijian@gmail.com>
2013-03-22 13:26:59 +0800

    Update init_yasnippet.el
    add python path

--- lisps/python/init_yasnippet.el
+++ lisps/python/init_yasnippet.el
@@ -1,9 +1,9 @@
 (message "start yasnippet")
 (add-to-list 'load-path
-             "~/emacs/lisp/yasnippet-0.6.1c")
+             "~/emacs/lisp/python/yasnippet-0.6.1c")
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas/initialize)
-(yas/load-directory "~/emacs/lisps/yasnippet-0.6.1c/snippets")
+(yas/load-directory "~/emacs/lisps/python/yasnippet-0.6.1c/snippets")
 
 (provide 'init_yasnippet)
 
