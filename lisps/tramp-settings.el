e54f8647092dc59402e67ac5bc5929aa97610f28
weikent <wei.shijian@gmail.com>
2013-03-23 22:54:32 +0800

    modify

--- lisps/tramp-settings.el
+++ lisps/tramp-settings.el
@@ -17,11 +17,27 @@
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
-(defun tramp-settings ()
-  "Settings of `tramp'."
-  (setq tramp-default-method "sudo"))
 
-(eval-after-load "tramp"
-  `(tramp-settings))
+
+
+
+;; (defun tramp-settings ()
+;;   "Settings of `tramp'."
+;;   (setq tramp-default-method "sudo"))
+
+;; (eval-after-load "tramp"
+;;   `(tramp-settings))
+
+
+(setq tramp-default-method "scp")
+(setq tramp-auto-save-directory "~/emacs/tramp")
+(setq auto-save-file-name-transforms
+       '(("\\`/[^/]*:\\(.+/\\)*\\(.*\\)" "/tmp/\\2")))
+(setq tramp-chunksize 328)
+(require 'ange-ftp)
+(require 'tramp)
+
+
+
 
 (provide 'tramp-settings)
