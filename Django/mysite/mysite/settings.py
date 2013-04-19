b9790575ea5c60b085ea46cec56c0ce338e6aa1a
weikent <wei.shijian@gmail.com>
2013-04-19 16:22:14 +0800

    Django 中添加第四章的代码

--- Django/mysite/mysite/settings.py
+++ Django/mysite/mysite/settings.py
@@ -1,3 +1,4 @@
+import os.path
 # Django settings for mysite project.
 
 DEBUG = True
@@ -111,6 +112,7 @@ ROOT_URLCONF = 'mysite.urls'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- Django/mysite/mysite/views.py
+++ Django/mysite/mysite/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+from django.template.loader import get_template
+from django.template import Context
 from django.http import Http404, HttpResponse
 import datetime
 
@@ -17,8 +20,13 @@ def current_datetime(request):
     - `request`:
     """
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" %now
-    return HttpResponse(html)
+    # t = get_template('current_datetime.html')
+    # html = t.render(Context({'current_date': now}))
+    # return HttpResponse(html)
+
+
+    return render(request, 'current_datetime.html', {'current_date':now})
+
 
 
 def hours_ahead(request, offset):
@@ -35,6 +43,7 @@ def hours_ahead(request, offset):
 
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
 
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    # html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
 
-    return HttpResponse(html)
+    # return HttpResponse(html)
+    return render(request, "hours_ahead.html",{"offset":offset,"dt":dt})
