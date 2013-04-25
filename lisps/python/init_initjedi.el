df3056dae827812d022fe7a385bea96e515b052e
weikent <wei.shijian@gmail.com>
2013-04-25 08:45:32 +0800

    添加对系统的判断，用来设置相应都clang 搜索路径

--- lisps/python/init_initjedi.el
+++ lisps/python/init_initjedi.el
@@ -11,10 +11,10 @@
 (add-hook 'python-mode-hook 'jedi:ac-setup)
 
 (add-hook 'python-mode-hook
-'(lambda ()
+		  '(lambda ()
 
-   (define-key python-mode-map [(meta s)] 'jedi:complete)
-))
+			 (define-key python-mode-map [(meta s)] 'jedi:complete)
+			 ))
 
 
 (add-to-list 'load-path "~/.emacs.d/el-get/pos-tip")
@@ -31,9 +31,9 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/el-get/auto-complete/dict")
 (ac-config-default)
-  
+
 (global-auto-complete-mode t)  
-  
+
 ;; 使用Ctrl+enter按键触发自动补全  
 (define-key ac-mode-map  [(control return)] 'auto-complete)  
 
@@ -54,11 +54,14 @@
 ;;(define-key ac-mode-map  [(control tab)] 'auto-complete)    
 ;; 提示快捷键为 M-/  
 ;; (define-key ac-mode-map  (kbd "M-/") 'auto-complete)   
-(defun my-ac-config ()    
-  (setq ac-clang-flags    
-        (mapcar(lambda (item)(concat "-I" item))    
-               (split-string    
-                "  
+
+
+(defun my-ac-config ()
+  (when (eq system-type 'darwin)
+	(setq ac-clang-flags    
+		  (mapcar(lambda (item)(concat "-I" item))    
+				 (split-string    
+				  "  
 /usr/llvm-gcc-4.2/bin/../lib/gcc/i686-apple-darwin11/4.2.1/include
  /usr/include/c++/4.2.1
  /usr/include/c++/4.2.1/backward
@@ -68,14 +71,31 @@
  /System/Library/Frameworks
  /Library/Frameworks
 "
-)))
+				  ))))
+  (when (eq system-type 'gnu/linux)
+	(setq ac-clang-flags    
+		  (mapcar(lambda (item)(concat "-I" item))    
+				 (split-string    
+				  "  
+/usr/llvm-gcc-4.2/bin/../lib/gcc/i686-apple-darwin11/4.2.1/include
+ /usr/include/c++/4.2.1
+ /usr/include/c++/4.2.1/backward
+ /usr/local/include
+ /Applications/Xcode.app/Contents/Developer/usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include
+ /usr/include
+ /System/Library/Frameworks
+ /Library/Frameworks
+"
+				  ))))
   (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))    
   (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)    
   (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)    
   (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)    
   (add-hook 'css-mode-hook 'ac-css-mode-setup)    
   (add-hook 'auto-complete-mode-hook 'ac-common-setup)    
-  (global-auto-complete-mode t))    
+  (global-auto-complete-mode t))
+
+ 
 (defun my-ac-cc-mode-setup ()    
   (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))    
 (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)    
