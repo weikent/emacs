fbd8aad98eb1b487979e9455329cb06e11da3b58
weikent <wei.shijian@gmail.com>
2013-08-15 18:59:46 +0800

    在init_yasnippet中，添加全局启动 (yas/global-mode 1)

--- lisps/python/init_yasnippet.el
+++ lisps/python/init_yasnippet.el
@@ -2,6 +2,7 @@
 (add-to-list 'load-path
              "~/emacs/lisp/python/yasnippet-0.6.1c")
 (require 'yasnippet) ;; not yasnippet-bundle
+(yas/global-mode 1)
 (yas/initialize)
 (yas/load-directory "~/emacs/lisps/python/yasnippet-0.6.1c/snippets")
 
