f61dff1438ef5cd0461e909d8cc6c62bc4b793b6
weikent <wei.shijian@gmail.com>
2013-09-12 16:48:57 +0800

    重新配置C-settings.el“

--- lisps/C-settings.el
+++ lisps/C-settings.el
@@ -10,37 +10,45 @@
 
 
 
-;; ;; C语言设置  
-;; (defun my-c-mode-hook ()  
-;;   (c-set-style "k&r"))  
-;; (add-hook 'c-mode-hook 'my-c-mode-hook)  
-;; ;; C++设置  
-;; (defun my-c++-mode-hook ()  
-;;   (c-set-style "stroustrup"))  
-;; (add-hook 'c++-mode-hook 'my-c++-mode-hook)  
-;; ;;(add-hook 'c-mode-hook 'my-c++-mode-hook) 
+;; C语言设置  
+(defun my-c-mode-hook ()  
+  (c-set-style "awk"))  
+(add-hook 'c-mode-hook 'my-c-mode-hook)  
+;; C++设置  
+(defun my-c++-mode-hook ()  
+  (c-set-style "stroustrup"))  
+(add-hook 'c++-mode-hook 'my-c-mode-hook)  
+(add-hook 'c-mode-hook 'my-c-mode-hook) 
 
 
+(defconst mystyle
+  '((setq indent-tabs-mode nil))
+  "mystyle" )
+(c-add-style "mystyle" mystyle)
+(defun myhook()
+  (setq indent-tabs-mode nil)
+  (c-set-style "mystyle"))
+(add-hook 'c++-mode-hook 'myhook)
+(add-hook 'c-mode-hook 'myhook)
 
+;; ;; (setq-default c-indent-tabs-mode t     ; Pressing TAB should cause indentation
+;; ;; 	      c-indent-level 4         ; A TAB is equivilent to four spaces
+;; ;; 	      c-argdecl-indent 0       ; Do not indent argument decl's extra
+;; ;; 	      c-tab-always-indent t
+;; ;; 	      backward-delete-function nil) ; DO NOT expand tabs when deleting
+;; ;; (c-add-style "my-c-style" '((c-continued-statement-offset 4))) ; If a statement continues on the next line, indent the continuation by 4
+;; ;; (defun my-c-mode-hook ()
+;; ;;   (c-set-style "my-c-style")
+;; ;;   (c-set-offset 'substatement-open '0) ; brackets should be at same indentation level as the statements they open
+;; ;;   (c-set-offset 'inline-open '+)
+;; ;;   (c-set-offset 'block-open '+)
+;; ;;   (c-set-offset 'brace-list-open '+)   ; all "opens" should be indented by the c-indent-level
+;; ;;   (c-set-offset 'case-label '+))       ; indent case labels by c-indent-level, too
+;; ;; (add-hook 'c-mode-hook 'my-c-mode-hook)
 
-(setq-default c-indent-tabs-mode t     ; Pressing TAB should cause indentation
-	      c-indent-level 4         ; A TAB is equivilent to four spaces
-	      c-argdecl-indent 0       ; Do not indent argument decl's extra
-	      c-tab-always-indent t
-	      backward-delete-function nil) ; DO NOT expand tabs when deleting
-(c-add-style "my-c-style" '((c-continued-statement-offset 4))) ; If a statement continues on the next line, indent the continuation by 4
-(defun my-c-mode-hook ()
-  (c-set-style "my-c-style")
-  (c-set-offset 'substatement-open '0) ; brackets should be at same indentation level as the statements they open
-  (c-set-offset 'inline-open '+)
-  (c-set-offset 'block-open '+)
-  (c-set-offset 'brace-list-open '+)   ; all "opens" should be indented by the c-indent-level
-  (c-set-offset 'case-label '+))       ; indent case labels by c-indent-level, too
-(add-hook 'c-mode-hook 'my-c-mode-hook)
-
-(add-hook 'c++-mode-hook
-          (lambda () (setq comment-start "/* " comment-end " */")))
-(add-hook 'c++-mode-hook 'my-c-mode-hook)
+;; ;; (add-hook 'c++-mode-hook
+;; ;;           (lambda () (setq comment-start "/* " comment-end " */")))
+;; ;; (add-hook 'c++-mode-hook 'my-c-mode-hook)
 
 
 
